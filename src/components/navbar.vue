9d99c1bb5d5d44de5d4763db7dd6e0bbe23bc816
sirbrian52 <ghanni.briantama12@gmail.com>
2024-08-14 17:19:17 +0700

    Initial commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+      <div class="burger" @click="toggleMenu">
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+      </div>
+      <div class="title">
+        <img src="../assets/img/sat.jpg" alt="" />
+        <div class="title-text">
+        <h1>Cora Platform</h1>
+        <h2>Advance Production Scheduling</h2>
+        </div>
+      </div>
+      <div class="user">
+        <span>Joko Siyanto - Production</span>
+        <img src="../assets/img/woman-avatar.png" alt="User Icon" />
+      </div>
+    <nav class="nav-menu":class="{ open: isMenuOpen }" id="nav-menu">
+      <span class="sidenav-close" @click="toggleMenu">&times;</span>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+    </header>
+</template>
+
+<script>
+export default{
+  name:'Navbar',
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen; 
+    },
+  },
+
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue"; 
+import App from "./App.vue";
+// // import Index from "./views/index.vue";
+// import "./assets/css/index.css"
+// // const routes = [
+// //   { path: "/", component: Index } 
+// // ];
+
+// // const router = create
+
+createApp(App).mount('#app')
